e39417173756c0bc92ff72a58a17172d70c84416
Brandon <bvance800@gmail.com>
2016-10-27 18:59:35 +0000

    Form with a submit button. intense stuff...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', {root: 'public'};
+});
+
+router.get('/animals', function(req, res, next){
+
+	alert("So you think you're special?");
+
 });
 
 module.exports = router;
