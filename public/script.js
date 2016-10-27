1722408d2a40d5534dac44c1915b05276d228d41
Allison Walke <awalke796@gmail.com>
2016-10-27 19:43:40 +0000

    js added

---
+++ public/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+        $( "#animal-form" ).submit(function(e) {
+        var keyword = $("#animal").val();
+        console.log(keyword);
+        e.preventDefault();
+
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+            tags: keyword,
+            tagmode: "any",
+            format: "json"
+        },
+        function(data) {
+            var image_src = data.items[0]['media']['m'].replace("_m", "_b");
+
+            $('body').css('background-image', "url('" + image_src + "')");
+            $('body').css('background-repeat', "no-repeat");
+            $('body').css('background-position', "center center");
+            $('body').css('background-attachment', "fixed");
+            $('body').css('background-size', "cover");
+
+        });
+    });
+
+    });
