17ebd166dfd939d75d4411ddf7660e8359855a46
Brandon <bvance800@gmail.com>
2016-10-27 20:23:55 +0000

    adding a filter.....

--- public/script.js
+++ public/script.js
@@ -1,26 +1,39 @@
 $(document).ready(function(){
 
         $( "#animal-form" ).submit(function(e) {
-        var keyword = $("#animal").val();
-        console.log(keyword);
-        e.preventDefault();
+        
+	var url = "getanimal?q=" + $("#animal").val();
+	console.log("in the .submit()");
+	e.preventDefault();
+
+	$.getJSON(url, function(data){
+		
+	console.log(data);	
+
+	if(data.length !== 0)
+	{
+		var keyword = $("#animal").val();
+        	console.log(keyword);
 
         $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        {
-            tags: keyword,
-            tagmode: "any",
-            format: "json"
-        },
-        function(data) {
-            var image_src = data.items[0]['media']['m'].replace("_m", "_b");
+        	{
+        	    	tags: keyword,
+            		tagmode: "any",
+           	 	format: "json"
+        	},
+       	 function(data) {
+            	var image_src = data.items[0]['media']['m'].replace("_m", "_b");
 
-            $('body').css('background-image', "url('" + image_src + "')");
-            $('body').css('background-repeat', "no-repeat");
-            $('body').css('background-position', "center center");
-            $('body').css('background-attachment', "fixed");
-            $('body').css('background-size', "cover");
+           	 $('body').css('background-image', "url('" + image_src + "')");
+           	 $('body').css('background-repeat', "no-repeat");
+           	 $('body').css('background-position', "center center");
+           	 $('body').css('background-attachment', "fixed");
+           	 $('body').css('background-size', "cover");
 
-        });
+        	});
+	}
+
+});
     });
 
     });
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,36 @@ router.get('/animals', function(req, res, next){
 
 });
 
+router.get('/getanimal', function(req, res, next){
+
+	var fs = require('fs');
+
+	fs.readFile(__dirname + '/animals.txt',function(err,data) 
+		{ 
+			if(err)
+			{
+				console.log("Oh my error...");
+				 throw err;
+			}
+			
+			var search = req.query.q;
+
+			var jsonresult = [];
+
+			var animals = data.toString().split("\n"); 
+			for(var i = 0; i < animals.length; i++) 
+			{	var result = animals[i];
+				if(result === search)
+				{ 
+					jsonresult.push({type:animals[i]});	 
+				}
+			} 
+
+			console.log(jsonresult);
+			res.status(200).json(jsonresult);
+		}); 
+
+});
+
+
 module.exports = router;
